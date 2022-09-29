f6a273b44229d55a245ad60c1ee76677aae7d9d9
Oluwasanmi Idowu <macbook@MacBook-Pro-2.local>
2022-09-29 23:52:49 +0100

    final commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '6tmg14mdy7';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-k335250r.us.auth0.com',
+  clientId: 'TDV6UW3zZPO4HijvsCIYh6uOvftjMxg1',
+  callbackUrl: 'http://localhost:3000/callback'
+};
